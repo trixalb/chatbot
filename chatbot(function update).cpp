30db397f3979f2f4b05a4f06310c847c236c4984
the programmer <88006978+mitodocpp@users.noreply.github.com>
2021-11-20 16:16:54 -0300

    Create chatbot(function update).cpp

---
+++ chatbot(function update).cpp
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+
+int main()
+{
+//variables
+//boolean
+bool loop = false;
+bool sleeping = false;
+//int
+int function = 0;
+//string
+string option = "nil";
+string function_processed = "nil";
+string bot_name = "nil";
+string your_name = "nil";
+//counter variables
+int counter;
+int printed;
+bool executing = false;
+//general Script
+cout << "Welcome!";
+function = 1;
+function_processed = "run_chatbot_code";
+cout << "\nhello, to start from a name for me: ";
+cin >> bot_name;
+cout << "\nbeautiful name, and what's your name?: ";
+cin >> your_name;
+cout << "Welcome, ";
+cout << your_name;
+cout << "\nSo, how about you take a look at my duties? (yes/no): ";
+cin >> option;
+if(option == "yes")
+{
+cout << "counter";
+cout << "\ncalculator";
+cout << "\ncomming soon...";
+cout << "\ncomming soon...";
+cout << "would you like to run any of them? (yes/no): ";
+cin >> option;
+if(option == "yes")
+{
+function = 569;
+function_processed = "run_function";
+}
+}
+if(option == "no")
+{
+cout << "would you like to close the Script? (yes/no): ";
+if(option == "yes")
+{
+return 0;
+}
+if(option == "no")
+{
+while(1 == 1)
+{
+cout << "aeuuioa984foutorgj8erhg89easjrtgoiaerht89erhgiugehriughrrgyjgdyghggrtatragatgrgrtdeeargieriojgisgedfsdgfradesgrrdggefaredggferdrgfgadergfgdadgffg";
+}
+}
+}
+if(option != "no" and option != "yes")
+{
+cout << "Error 404";
+cout << "\noption invalidates";
+return 0;
+}
+if(function == 569 and function_processed == "run_function")
+{
+cout << "what is the name of the function?";
+cin >> option;
+if(option == "counter")
+{
+cout << "\nfunction selected: counter";
+cout << "\nfunction messages:";
+cout << "\n";
+cout << "enter the maximum number: ";
+cin >> counter;
+counter += 1;
+executing = true;
+while(executing == true)
+{
+cout << printed;
+cout << "\n";
+printed += 1;
+counter -= 1;
+if(counter == 0 || counter < 0)
+{
+cout << "the Script of the function has been terminated, would you like to finish running this Scrpt? (yes/no):";
+cin >> option;
+if(option == "no")
+{
+while(1 == 1)
+{
+cout << "spokgfokaropgkoisjgmoiszjdioajsg iojsiogjsdiofjoisdjgiokokspodkfoidjfiogjdoikgsodkfiogjskoji jsiofjosijdfiodfjg odijgodjgoidjfogis";
+}
+}
+if(option == "yes")
+{
+return 0;
+return 0;
+return 0;
+return 0;
+}
+}
+}
+}
+}
+}
